3c8e88f52c4fd357f2b3afe35e19a9bcad37d78a
Minki, Kim <xowhdltjd@gmail.com>
2021-04-15 17:52:55 +0900

    make base.html

--- pragmatic/accountapp/views.py
+++ pragmatic/accountapp/views.py
@@ -1,9 +1,7 @@
+# Create your views here.
+
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
-
-
 def hello_world(request):
-    return HttpResponse('Hello World!')
-
+    return render(request, 'base.html')
